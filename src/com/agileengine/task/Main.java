cbaf9351e39d13ddeb74adacfc7526b494af38a7
gmoly <mike.grinkov@gmail.com>
2018-07-12 21:17:19 +0300

    Initial commit

---
+++ src/com/agileengine/task/Main.java
@@ -0,0 +1,12 @@
+package com.agileengine.task;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
